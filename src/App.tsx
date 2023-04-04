67f4dd61f451f674c25b863250c5401b7eedfb1c
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2023-04-04 16:57:07 +0800

    Add dialog.

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,144 @@
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
   Button,
   Checkbox,
+  Container,
   FormControl,
   FormLabel,
   Input as ChakraInput,
   NumberInput,
   NumberInputField,
   Select,
+  useDisclosure,
   VStack,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { Field, Form, Formik } from "formik";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { cat_column_options, convert, Input, TrueInput } from "./types";
 
 const API_URL = "YOUR_API_URL_HERE";
 
 const App: React.FC = () => {
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [loading, setLoading] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState("");
+
   const handleSubmit = async (values: Partial<Input>) => {
+    setLoading(true);
+    onOpen();
     const res: TrueInput = convert(values);
     try {
       const response = await axios.post(API_URL, { data: res });
       if (response.data) {
         // Show success message
-        alert("Success: " + response.data);
+        setDialogMessage("Success");
       } else {
         // Show error message
-        alert("Error");
+        setDialogMessage("Error");
       }
     } catch (error: any) {
       // Show error message
-      alert("Error: " + error.message);
+      setDialogMessage("Error: " + error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      {() => (
-        <Form>
-          <VStack spacing={4}>
-            <>
-              {[
-                "title",
-                "location",
-                "department",
-                "company_profile",
-                "description",
-                "requirements",
-                "benefits",
-              ].map((name) => (
-                <TextField key={name} name={name} />
-              ))}
-            </>
+    <>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {() => (
+          <Form>
+            <Container
+              maxW="container.md"
+              p={{ base: 4, md: 8 }}
+              mt={{ base: 8, md: 16 }}
+            >
+              <VStack spacing={4}>
+                <>
+                  {[
+                    "title",
+                    "location",
+                    "department",
+                    "company_profile",
+                    "description",
+                    "requirements",
+                    "benefits",
+                  ].map((name) => (
+                    <TextField key={name} name={name} />
+                  ))}
+                </>
 
-            <>
-              {["salary_lower", "salary_upper"].map((name) => (
-                <NumberField key={name} name={name} />
-              ))}
-            </>
+                <>
+                  {["salary_lower", "salary_upper"].map((name) => (
+                    <NumberField key={name} name={name} />
+                  ))}
+                </>
 
-            <>
-              {["telecommuting", "has_company_logo", "has_questions"].map(
-                (name) => (
-                  <CheckBox key={name} name={name} />
-                )
-              )}
-            </>
+                <>
+                  {["telecommuting", "has_company_logo", "has_questions"].map(
+                    (name) => (
+                      <CheckBox key={name} name={name} />
+                    )
+                  )}
+                </>
 
-            <>
-              {Object.entries(cat_column_options).map(
-                ([categoryName, choices]) => (
-                  <ChoiceField
-                    key={categoryName}
-                    name={categoryName}
-                    choices={choices}
-                  />
-                )
-              )}
-            </>
+                <>
+                  {Object.entries(cat_column_options).map(
+                    ([categoryName, choices]) => (
+                      <ChoiceField
+                        key={categoryName}
+                        name={categoryName}
+                        choices={choices}
+                      />
+                    )
+                  )}
+                </>
 
-            <Button type="submit" colorScheme="blue">
-              Submit
-            </Button>
-          </VStack>
-        </Form>
-      )}
-    </Formik>
+                <Button type="submit" colorScheme="blue">
+                  Submit
+                </Button>
+              </VStack>
+            </Container>
+          </Form>
+        )}
+      </Formik>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={closeButtonRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              {loading ? "Loading..." : dialogMessage}
+            </AlertDialogHeader>
+
+            <AlertDialogBody>{loading ? "Please wait..." : ""}</AlertDialogBody>
+
+            {!loading && (
+              <AlertDialogFooter>
+                <Button
+                  ref={closeButtonRef}
+                  colorScheme="blue"
+                  onClick={onClose}
+                >
+                  Close
+                </Button>
+              </AlertDialogFooter>
+            )}
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
 
